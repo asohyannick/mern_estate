8ff1ea2d1ceeca679e93f398fc279de65830b0d5
asohyannick <asohyannick3@gmail.com>
2024-02-28 02:22:28 -0800

    Create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import User from '../models/user.model.js';
+import { StatusCodes } from 'http-status-codes';
+import bcryptjs from 'bcryptjs';
+export const signup = async(req, res, next) => {
+    const {username, email, password} = req.body;
+    const hashedPaasword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({username, email, password:hashedPaasword});
+    try {
+        await newUser.save();
+        res.status(StatusCodes.CREATED).json('User created successfully!');
+    } catch(error) {
+        console.log(error.message);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js'
 dotenv.config();
 const app = express();
 // security packages
 app.use(cors());
 // middleware packages
 app.use(express.json());
+app.use(cookieParser());
 // db config
 mongoose
   .connect(process.env.MONGO_URI)
@@ -17,6 +21,10 @@ mongoose
 app.get("/test", function (req, res) {
   res.send("<h1>API is working successfully</h1>");
 });
+
+// routes
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 8000;
 
