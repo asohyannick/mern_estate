570918e4b3b9428206fac95f41d2417564877286
asohyannick <asohyannick3@gmail.com>
2024-03-01 03:57:48 -0800

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,28 +1,58 @@
 import Listing from "../models/listing.model.js";
 import { StatusCodes } from "http-status-codes";
-import jwt from 'jsonwebtoken';
-import {errorHandler} from '../utils/error.js'
-export const createListing = async(req, res, next) => {
-    try {
-       const listing = await Listing.create(req.body);
-       return res.status(StatusCodes.CREATED).json(listing)
-    } catch(error) {
-      next(error)   
-    }
-}
-
-export const deleteListing = async(req, res, next) => {
-  const listing = await Listing.findById(req.params.id);
-  if(!listing) {
-    return next(errorHandler(StatusCodes.NOT_FOUND, 'Listing not found!'));
-  }
-  if(req.user.id !== listing.userRef) {
-    return next(errorHandler(StatusCodes.UNAUTHORIZED, 'You can only delete your own listings!'));
-  } 
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+export const createListing = async (req, res, next) => {
   try {
-     await Listing.findByIdAndDelete(req.params.id);
-     res.status(StatusCodes.OK).json('Listing has been deleted successfully!');
+    const listing = await Listing.create(req.body);
+    return res.status(StatusCodes.CREATED).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(StatusCodes.NOT_FOUND, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(
+        StatusCodes.UNAUTHORIZED,
+        "You can only delete your own listings!"
+      )
+    );
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(StatusCodes.OK).json("Listing has been deleted successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(StatusCodes.NOT_FOUND, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(
+        StatusCodes.UNAUTHORIZED,
+        "You can only update your own listing"
+      )
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true}
+    );
+    res.status(StatusCodes.OK).json(updatedListing)
   } catch(error) {
     next(error);
   }
-}
+};
